f9554f94e7d2126ccba137e41c282fb610af86b4
xms <544437819@qq.com>
2019-06-18 17:38:43 +0800

    将StateDispatchBloc设置为abstract

--- lib/bloc/index.dart
+++ lib/bloc/index.dart
@@ -3,9 +3,9 @@ import 'package:bloc/bloc.dart';
 export 'package:bloc/bloc.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
 
-/// [StateDispatchBloc]是专门用于传递数据的[Bloc]，即[bloc.dispatch]所接收的参数就是我们的[State]
-class StateDispatchBloc<T> extends Bloc<T, T> {
-  StateDispatchBloc(this._initialState) : super();
+/// [_StateDispatchBloc]是专门用于传递数据的[Bloc]，即[bloc.dispatch]所接收的参数就是我们的[State]
+abstract class _StateDispatchBloc<T> extends Bloc<T, T> {
+  _StateDispatchBloc(this._initialState) : super();
 
   final T _initialState;
 
@@ -18,4 +18,9 @@ class StateDispatchBloc<T> extends Bloc<T, T> {
     // 只有当我们需要传递一个事件，然后我们要做一些其他操作时，才使用bloc demo 中switch(event)的那种方式
     yield state;
   }
+}
+
+/// 首页-图片列表中双击可以全局保存当前用户喜欢的照片，并显示心型符号
+class FavorateBloc extends _StateDispatchBloc<List<String>> {
+  FavorateBloc(): super([]);
 }
